a2415edc62e9c7cb0599c0a4eb1ada49f7840cbd
Lss <47353127@qq.com>
2020-06-19 14:03:57 +0800

    测试3

---
+++ cloud/sp03-userservie/src/main/java/cn/tedu/sp03/Sp03UserservieApplication.java
@@ -0,0 +1,15 @@
+package cn.tedu.sp03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sp03UserservieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp03UserservieApplication.class, args);
+    }
+
+}
---
+++ cloud/sp09-fegin/src/main/java/cn/tedu/sp09/Sp09FeginApplication.java
@@ -0,0 +1,20 @@
+package cn.tedu.sp09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+//@EnableCircuitBreaker
+//@EnableDiscoveryClient
+//@SpringBootApplication
+@SpringCloudApplication
+@EnableFeignClients
+public class Sp09FeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp09FeginApplication.class, args);
+    }
+
+}
+
+
